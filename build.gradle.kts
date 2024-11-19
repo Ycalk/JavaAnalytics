964f4d6b0ee8549b152f20a1542ac2bbaf48ebb9
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-11-19 20:37:47 +0500

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.vk.api:sdk:1.0.16")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.slf4j:slf4j-api:1.7.36")
 }
 
 tasks.test {
